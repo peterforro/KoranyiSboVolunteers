b9c0ac585767f1d81133c70a1c1f853d68066f7e
peterforro <forropcs@gmail.com>
2020-11-07 23:40:45 +0100

    test greetings

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET"])
 def index():
-    print("\n\n\n!!!!!!!!!!!!Teszt trace log!!!!!!!!!!!!\n\n\n")
-    return "<h1>Hello World!</h1>"
-
+    name = request.args.get("name", "")
+    return f"<h1>Hello {name}!!!</h1>"
 
 
 if __name__ == "__main__":
-    app.run(
-        debug = True
-    )
+    app.run()
